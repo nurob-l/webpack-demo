8cc14bf3b2ef518cf1c2dd22f3635ffc6ea5308a
nurob-l <359150161@qq.com>
2019-12-23 22:30:39 +0800

    模块热替换章节 结束 使用webpack-dev-middleware

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,33 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: {
-    app: './src/index.js',
-    print: './src/print.js'
-  },
+  context: path.resolve(__dirname, './'),
+  entry: [
+    './src/index.js',
+    'webpack-hot-middleware/client'
+  ],
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Output Management'
-    })
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ],
   output: {
     filename: '[name].bundle.js',
